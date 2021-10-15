4a6d51de9b2a5a04aa23788021d035776cf96a2d
alexjones <ajonzy@gmail.com>
2021-10-15 15:22:02 -0600

    Added CORS

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
@@ -10,6 +11,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'ap
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+CORS(app)
 
 
 class Item(db.Model):
